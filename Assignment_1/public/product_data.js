60da79ba04549d6d0e0e2a46b6bfd0756c774cc9
Julia Kelliher <juliavfk@hawaii.edu>
2019-11-14 22:13:29 -1000

    assignment 1

---
+++ Assignment_1/public/product_data.js
@@ -0,0 +1,36 @@
+product_data = [
+    {
+        "type": "Classic",
+        "price": 1000.00,
+        "image": "./images/classic.jpg",
+        "description": "A California classic board for the classic Californian."
+    },
+    {
+        "type": "Noserider",
+        "price": 1200.00,
+        "image": "./images/noserider.jpg",
+        "description": "The noserider, perfect for walking on those glassy mornings"
+    },
+    {
+        "type": "Pintail",
+        "price": 1250.00,
+        "image": "./images/pintail.jpg",
+        "description": "Want to catch those barrels? Dive in with this guy"
+    },
+    {
+        "type": "Square Tail",
+        "price": 1100.00,
+        "image": "./images/square_tail.jpg",
+        "description": "You're gonna love it"
+    },
+    {
+        "type": "Triple Stringer",
+        "price": 1450.00,
+        "image": "./images/triple_stringer.jpg",
+        "description": "The California oldschool look"
+    }
+];
+
+if (typeof module !='undefined') {
+    module.exports=product_data;
+}
